a3c1b7b20ec734228f27ea3f493d16a768b2d139
unknown <nicholas.gebhart@gmail.com>
2019-09-06 08:23:56 -0700

    Added header MVP

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,29 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const entryPoint = document.querySelector(".header-container");
 
-function Header() {}
+function Header() {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temp = document.createElement("span");
+
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+
+  date.textContent = "MARCH 28, 2019";
+  title.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  return header;
+}
+
+const newHeader = Header();
+
+entryPoint.appendChild(newHeader);
