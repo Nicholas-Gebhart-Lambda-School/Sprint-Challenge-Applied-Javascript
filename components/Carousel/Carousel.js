db50dc41412f62b87a64cc32cb52e950c6886b7f
unknown <nicholas.gebhart@gmail.com>
2019-09-06 09:34:44 -0700

    Added carousel stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,51 @@
     <div class="right-button"> > </div>
   </div>
 */
+const carouselEntry = document.querySelector(".carousel-container");
+const carouselArray = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+];
+let temp = 0;
+let count = 1;
+const Carousel = arr => {
+  const carousel = document.createElement("div");
+  const leftButton = document.createElement("div");
+  const img = document.createElement("img");
+  const rightButton = document.createElement("div");
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(rightButton);
+  carousel.appendChild(img);
+
+  carousel.classList.add("carousel");
+  leftButton.classList.add("left-button");
+  rightButton.classList.add("right-button");
+
+  leftButton.textContent = " < ";
+  rightButton.textContent = " > ";
+  img.src = arr[temp];
+
+  leftButton.addEventListener("click", e => {
+    img.src = arr[count];
+    count--;
+    if (count === -1) {
+      count = arr.length - 1;
+    }
+  });
+
+  rightButton.addEventListener("click", e => {
+    img.src = arr[count];
+    count++;
+    if (count === arr.length) {
+      count = 0;
+    }
+  });
+
+  return carousel;
+};
+
+// Carousel(carouselArray)
+carouselEntry.appendChild(Carousel(carouselArray));
